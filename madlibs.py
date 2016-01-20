39c66c8a9e8920925500ecfb1221f81dff3e97dc
Hackbright Student <info@hackbrightacademy.com>
2016-01-19 16:51:51 -0800

    Working MADLIBS game!

---
+++ madlibs.py
@@ -0,0 +1,72 @@
+from random import choice
+
+from flask import Flask, render_template, request
+
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
+    'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+
+
+@app.route('/')
+def start_here():
+    """Homepage."""
+
+    return "Hi! This is the home page."
+
+
+@app.route('/hello')
+def say_hello():
+    """Save hello to user."""
+
+    return render_template("hello.html")
+
+
+@app.route('/greet')
+def greet_person():
+    """Greet user."""
+
+    player = request.args.get("person")
+
+    compliment = choice(AWESOMENESS)
+
+    return render_template("compliment.html",
+                           person=player,
+                           compliment=compliment)
+
+
+@app.route('/game')
+def show_game_form():
+    """Render Goodbye or, if the player wishes to play a game, ask madlibs game questions."""
+
+    response = request.args.get("playgame")
+
+    if response == "no":
+        return render_template("goodbye.html")
+    elif response == "yes":
+        return render_template("game.html")
+
+
+@app.route('/madlib')
+def show_madlib():
+
+    person_response = request.args.get("person")
+    color_response = request.args.get("color")
+    noun_response = request.args.get("noun")
+    adjective_response =request.args.get("adjective")
+
+    return render_template("madlib.html",
+                       person=person_response,
+                       color=color_response,
+                       noun=noun_response,
+                       adjective=adjective_response)
+    
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads" our web app
+    # if we change the code.
+    app.run(debug=True)
